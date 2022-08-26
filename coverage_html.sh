004c810ffb28d49790cfc54e6548ce4c5008dc36
Erastus Murungi <erastuswagwan@gmail.com>
2022-08-26 12:55:44 -0700

    minor script modifications

--- coverage_html.sh
+++ coverage_html.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+GREEN='\x1B[32;1m'
+RESET='\x1B[0m' # No Color
+printf "Running %s cmake with PROFILE=1 $%s" "$GREEN" "$RESET"
 cmake CMakeLists.txt -DPROFILE=1
 make
 ctest
-/usr/local/opt/llvm/bin/llvm-cov gcov "./CMakeFiles/strassen*/*.gcno"
+/usr/local/opt/llvm/bin/llvm-cov gcov ./CMakeFiles/strassen*/*.gcno
 mkdir "temp"
 gcovr --html-details temp/output.html
 open temp/output.html
-rm -rf "temp"
